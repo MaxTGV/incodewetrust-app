56803974456bc6fceb52472c9bcd81ac8e2cddbc
MaxTGV <maxtugovlife@gmail.com>
2021-05-07 18:08:25 +0600

    added functionality for adding / removing quotes to the localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { favoriteQuoteSlice } from "./state/favoriteQuoteSlice";
-import { getQuote, getFavoriteQuote } from "./state/selectors";
+import { getFavoriteQuote, getQuote } from "./state/selectors";
 import { fetchQuote } from "./state/thunk";
 
 export const App = () => {
   const [isCliked, setIsCliked] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
+
   const dispatch = useDispatch();
   const quote = useSelector(getQuote);
   const favoriteQuoteList = useSelector(getFavoriteQuote);
-  console.log(favoriteQuoteList);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -29,11 +29,15 @@ export const App = () => {
 
   const favoriteQuote = () => {
     setIsFavorite(!isFavorite);
-    console.log(isFavorite);
     if (!isFavorite) {
       return dispatch(favoriteQuoteSlice.actions.addQuote(quote));
     }
     return dispatch(favoriteQuoteSlice.actions.removeQuote(quote));
+  };
+
+  const clearAll = () => {
+    setIsFavorite(false);
+    dispatch(favoriteQuoteSlice.actions.clearQuoteList());
   };
 
   return (
@@ -45,6 +49,7 @@ export const App = () => {
       <button onClick={() => favoriteQuote()}>
         {isFavorite ? "Убрать из любимых" : "В любимые"}
       </button>
+      <button onClick={() => clearAll()}>Удалить все</button>
       <div>------------------------</div>
       {quote && <div>{quote.value}</div>}
       <div>------------------------</div>
--- src/state/favoriteQuoteSlice.js
+++ src/state/favoriteQuoteSlice.js
@@ -2,17 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const favoriteQuoteSlice = createSlice({
   name: "favoriteQuote",
-  initialState: { data: [] },
+  initialState: { data: JSON.parse(localStorage.getItem("favorite")) || [] },
   reducers: {
     addQuote: (state, action) => {
-      return { data: [...state.data, action.payload] }; //state.data.push(action.payload);
+      localStorage.setItem(
+        "favorite",
+        JSON.stringify([...state.data, action.payload])
+      );
+      return { data: [...state.data, action.payload] };
     },
     removeQuote: (state, action) => {
       const newFavoriteQuoteList = state.data.filter(
         (quote) => quote.id !== action.payload.id
       );
-      return {data: newFavoriteQuoteList};
+      localStorage.setItem("favorite", JSON.stringify(newFavoriteQuoteList));
+      return { data: newFavoriteQuoteList };
     },
-    clearQuoteList: (state, action) => {},
+    clearQuoteList: () => {
+      localStorage.setItem("favorite", JSON.stringify([]));
+      return { data: [] };
+    },
   },
 });
